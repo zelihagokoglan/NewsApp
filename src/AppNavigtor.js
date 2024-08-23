e3ee4e54ed35e105970e370d1795da3bc8421862
serdargavas <serdargavas@gmail.com>
2024-08-23 20:54:01 +0300

    clerk

--- src/AppNavigtor.js
+++ src/AppNavigtor.js
@@ -1,48 +1,46 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import OnBoardingScreen from './screens/OnBoardingScreen';
-import TabNavigator from './TabNavigator';
-import NewsDetailScreen from './screens/NewsDetailScreen'; 
-import  FavoritesContextProvider  from '../store/FavoritesContext';
-import { ClerkProvider,ClerkLoaded } from '@clerk/clerk-expo';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import OnBoardingScreen from "./screens/OnBoardingScreen";
+import TabNavigator from "./TabNavigator";
+import NewsDetailScreen from "./screens/NewsDetailScreen";
+import FavoritesContextProvider from "../store/FavoritesContext";
+import { ClerkProvider, ClerkLoaded } from "@clerk/clerk-expo";
 
 const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
-if(!publishableKey){
+if (!publishableKey) {
   throw new Error("Missing Publishable Key.");
 }
-
 
 const Stack = createStackNavigator();
 
 const AppNavigator = () => {
   return (
-    <ClerkProvider publishableKey='publishableKey'>
-     <ClerkLoaded>
-      <NavigationContainer>
-       <FavoritesContextProvider> 
-         <Stack.Navigator initialRouteName="OnBoardingScreen">
-          <Stack.Screen 
-            name="OnBoardingScreen" 
-            component={OnBoardingScreen} 
-            options={{ headerShown: false }} 
-          />
-          <Stack.Screen 
-            name="TabNavigator" 
-            component={TabNavigator} 
-            options={{ headerShown: false }} 
-          />
-          <Stack.Screen 
-            name="NewsDetailScreen" 
-            component={NewsDetailScreen} 
-            options={{ headerShown: true, title: 'Detay' }} 
-          />
-        </Stack.Navigator>
-       </FavoritesContextProvider>
-      </NavigationContainer>
-     </ClerkLoaded>
+    <ClerkProvider publishableKey={publishableKey}>
+      <ClerkLoaded>
+        <NavigationContainer>
+          <FavoritesContextProvider>
+            <Stack.Navigator initialRouteName="OnBoardingScreen">
+              <Stack.Screen
+                name="OnBoardingScreen"
+                component={OnBoardingScreen}
+                options={{ headerShown: false }}
+              />
+              <Stack.Screen
+                name="TabNavigator"
+                component={TabNavigator}
+                options={{ headerShown: false }}
+              />
+              <Stack.Screen
+                name="NewsDetailScreen"
+                component={NewsDetailScreen}
+                options={{ headerShown: true, title: "Detay" }}
+              />
+            </Stack.Navigator>
+          </FavoritesContextProvider>
+        </NavigationContainer>
+      </ClerkLoaded>
     </ClerkProvider>
-    
   );
 };
 
